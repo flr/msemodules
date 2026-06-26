c726afe5302bcb777a409bb1a7f62f2d15fa1a17
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2026-06-26 18:29:13 +0200

    Moved decision() from mse

--- R/tracking.R
+++ R/tracking.R
@@ -179,3 +179,74 @@ trackingFLQuants <- function(x) {
 
   return(res)
 }
+
+# decisions {{{
+
+#' @examples
+#' data(plesim)
+
+decisions <- function(x, years=dimnames(tracking(x))$year, iter=NULL) {
+
+  # EXTRACT tracking and args
+  trac <- tracking(x)
+  args <- args(x)
+
+  # USE year as numeric
+  years <- as.numeric(years)
+
+  # SET iters if not given
+  if(is.null(iter))
+    iter <- seq(dims(x)$iter)
+
+  # FUNCTION to compute table along years
+  .table <- function(d) {
+
+    its <- dims(d)$iter
+    dmns <- dimnames(d)
+
+    if(its == 1) {
+      data.frame(metric=dmns$metric, year=dmns$year, value=prettyNum(d))
+    } else {
+      data.frame(metric=dmns$metric, year=dmns$year,
+        value=sprintf("%s (%s)", 
+          prettyNum(apply(d, 1:5, median, na.rm=TRUE)),
+          prettyNum(apply(d, 1:5, mad, na.rm=TRUE))))
+    }
+  }
+
+  # COMPUTE tables
+  res <- lapply(years, function(y) {
+
+    # GET advice, data and management years
+    ay  <-  y
+    dy <- ay - args$data_lag
+    my  <- ay + args$management_lag
+
+    # SET metrics to extract
+
+    # data
+    dmet <- c("SB.om", "SB.obs", "SB.est", "met.hcr")
+    dmet <- dmet[dmet %in% dimnames(trac)$metric]
+
+    # advice
+    amet <- c("decision.hcr", "fbar.hcr", "hcr", "fbar.isys", "isys",
+      "fwd", "C.om")
+
+    amet <- amet[amet %in% dimnames(trac)$metric]
+
+    # SUBSET metrics from tracking
+    dout <- trac[dmet, ac(dy),,,, iter]
+    aout <- trac[amet, ac(ay),,,, iter]
+
+    # COMPUTE diff metrics
+    mout <- trac["SB.om", ac(my),,,,iter] / trac["SB.om", ac(ay),,,,iter]
+    dimnames(mout)$metric <- "diff(SB.om)"
+
+    # BIND into single table
+    rbind(.table(dout), .table(aout), .table(mout))      
+  })
+
+  do.call(cbind, res)
+}
+# }}}
+
