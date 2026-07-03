1e8e097f30761949a125c94c8cc7727752f31aef
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2026-07-03 20:23:52 +0200

    Option to add computed periods to annual table

--- R/performance.R
+++ R/performance.R
@@ -666,7 +666,7 @@ setLabelPerformance <- function(file="model/performance.dat.gz", labels) {
 #'   the years belonging to each period.
 #' @keywords manip
 
-periodsPerformance <- function(x, periods=list(), ...) {
+periodsPerformance <- function(x, periods=list(), add=TRUE, ...) {
 
   # COLLECT any named ... args (e.g. short=2026:2030, long=2031:2040)
   dots <- list(...)
@@ -709,6 +709,9 @@ periodsPerformance <- function(x, periods=list(), ...) {
 
   # RENAME col to  years
   setnames(res, "year", "years")
+
+  if(add)
+    res <- rbindlist(list(x, res), fill=TRUE)
 
   return(res)
 }
